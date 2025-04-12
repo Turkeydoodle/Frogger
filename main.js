406d8b4561bab8ecf0c0504bf42e7db2ff87291e
Turkeydoodle <kevintoddyu@gmail.com>
2025-04-12 07:04:35 -1000

    a preview with player movement

--- main.js
+++ main.js
@@ -22,12 +22,48 @@ for (let i = 0; i < 10; i++) {
         boxes.push(row1box);    
     }
 }
-function update() {
-    context.clearRect(0, 0, c.width, c.height);  
-    for (let i = 0; i < boxes.length; i++) {
-        move(boxes[i]);  
-        boxes[i].draw(); 
+let player = {
+    x: c.width / 2 - 10,
+    y: c.height - 30,
+    width: 20,
+    height: 20,
+    color: 'blue',
+    speed: 5,
+    draw: function() {
+        context.fillStyle = this.color;
+        context.fillRect(this.x, this.y, this.width, this.height);
     }
+};
+
+let keys = {};
+document.addEventListener('keydown', function(e) {
+    keys[e.key] = true;
+});
+document.addEventListener('keyup', function(e) {
+    keys[e.key] = false;
+});
+const blockSize = 20;
+
+function update() {
+    context.clearRect(0, 0, c.width, c.height);
+    let intendedX = player.x;
+    let intendedY = player.y;
+    if (keys['ArrowLeft'] || keys['a']) {
+        intendedX -= blockSize; }
+    if (keys['ArrowRight'] || keys['d']) {
+        intendedX += blockSize; }
+    if (keys['ArrowUp'] || keys['w']) {
+        intendedY -= blockSize;}
+    if (keys['ArrowDown'] || keys['s']) {
+        intendedY += blockSize;}
+    if (intendedX >= 0 && intendedX <= c.width - player.width) {
+        player.x = intendedX;}
+    if (intendedY >= 0 && intendedY <= c.height - player.height) {
+        player.y = intendedY;}
+    player.draw();
+    for (let i = 0; i < boxes.length; i++) {
+        move(boxes[i]);
+        boxes[i].draw();}
     requestAnimationFrame(update);
 }
 update();
